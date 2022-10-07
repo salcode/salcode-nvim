e1b78925523286d74010ad3f90d2bf7c0d058b21
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-10-06 20:48:47 -0400

    Add mappings for switching indent styles

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,3 +1,12 @@
 local set = vim.keymap.set
 
 vim.g.mapleader = " "
+
+-- Mappings to indent with tabs or spaces.
+------------------------------------------
+-- Set indentation to 2 spaces.
+set("n", "<leader>ts2", ":set tabstop=2 softtabstop=2 shiftwidth=2 expandtab<CR>")
+-- Set indentation to 4 spaces.
+set("n", "<leader>ts4", ":set tabstop=4 softtabstop=4 shiftwidth=4 expandtab<CR>")
+-- Set indentation to tabs, rendered as size 4 spaces.
+set("n", "<leader>tt4", ":set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab<CR>")
