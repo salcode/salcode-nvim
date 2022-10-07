c64f8623e3009d109189ace74f489f400043bb32
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-10-06 20:31:21 -0400

    Add options to options.lua
    Generally based on
    https://github.com/salcode/salcode-vim/blob/87adacbed5ced25bfad6599d1b3f923ae977d690/init.vim#L6-L20

--- lua/options.lua
+++ lua/options.lua
@@ -4,5 +4,17 @@ vim.g.mapleader = " "
 
 local o = vim.opt
 o.clipboard = "unnamedplus"
-
-
+o.expandtab = false
+o.shiftwidth = 4
+o.tabstop = 4
+o.relativenumber = true
+o.number = true
+o.termguicolors = true
+o.swapfile = false
+o.showmode = false
+o.list = true
+o.listchars = { tab = '> ', trail = '-', extends = '>', precedes = '<' , nbsp = '+' }
+o.ignorecase = true
+o.smartcase = true
+o.hlsearch = true
+o.incsearch = true
