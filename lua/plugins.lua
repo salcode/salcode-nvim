677ac942ba8bcad5108fce36082a5942680a4358
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-11-19 07:52:04 -0500

    On changes to plugins, source file and PackerSync
    Add autocmd to detect when the plugins file is modified. When this
    occurs, source the file and run PackerSync

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,6 +21,14 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.api.nvim_command("packadd packer.nvim")
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 -- initialize and configure packer
 local packer = require("packer")
 
