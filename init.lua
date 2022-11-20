9dd976b481ce3bd63bf17c672b950c9c3648b7b8
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-11-19 22:32:42 -0500

    Add highlight on yank
    When doing something like 2yaw, you'll see the two yanked works
    highlighted briefly.

--- init.lua
+++ init.lua
@@ -2,3 +2,12 @@ require("commands")
 require("mappings")
 require("plugins")
 require("options")
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({timeout = 250})
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
