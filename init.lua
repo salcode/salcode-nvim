242369896e94d2c802a401384e9e033ed239e78f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-10-22 12:49:26 -0400

    Load and set dracula colorscheme

--- init.lua
+++ init.lua
@@ -17,4 +17,14 @@ vim.g.mapleader = " "
 
 require("options")
 require("mappings")
-require("lazy").setup({})
+require("lazy").setup({
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme dracula]])
+		end,
+	},
+})
