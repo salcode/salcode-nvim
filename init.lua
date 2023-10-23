f838398b331112180bff2b0fe66dd0fea68081eb
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-10-23 06:46:11 -0400

    Add lewis6991/gitsigns.nvim plugin

--- init.lua
+++ init.lua
@@ -32,6 +32,44 @@ require("lazy").setup({
 	"editorconfig/editorconfig-vim",
 	"justinmk/vim-dirvish",
 	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				-- don't override the built-in and fugitive keymaps
+				local gs = package.loaded.gitsigns
+				vim.keymap.set({ 'n', 'v' }, ']c', function()
+					if vim.wo.diff then
+						return ']c'
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+				vim.keymap.set({ 'n', 'v' }, '[c', function()
+					if vim.wo.diff then
+						return '[c'
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+
+				vim.keymap.set('n', '<leader>gb', function() gs.blame_line{full=true} end)
+			end,
+		},
+	},
+	{
 		"tpope/vim-commentary",
 		config = function()
 			vim.api.nvim_create_autocmd(
