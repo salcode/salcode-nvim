20a415508f9c90994e7ac053bf75650d6f7e9812
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-09-08 16:43:34 -0400

    Move dracula colorscheme config into separate file
    See #20

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,4 @@
 return {
-	{
-		"Mofiqul/dracula.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme dracula]])
-		end,
-	},
 	"christoomey/vim-tmux-navigator",
 	{
 		"dense-analysis/ale",
