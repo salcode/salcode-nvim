140737e290fe30d8aea4e89f7ec96e65b6391568
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-11-04 17:35:34 -0500

    Skip confirm for simple edits

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,9 @@ return {
 	"stevearc/oil.nvim",
 	config = function()
 		local oil = require("oil")
-		oil.setup()
+		oil.setup({
+			skip_confirm_for_simple_edits = true,
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
