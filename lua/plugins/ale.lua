bf829a2a5456a4b963672ffe43b40869c478f026
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2025-10-09 21:24:49 -0400

    Disable phpstan in ALE
    Disable phpstan in ALE to prevent "Function not found" error messages.
    
    Resolves #30

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -1,3 +1,8 @@
 return {
 	"dense-analysis/ale",
+	config = function()
+		-- Disable PHPStan linter in ALE. Temporary workaround for project throwing "Function not found." errors.
+		-- See: https://github.com/salcode/salcode-nvim/issues/30
+		vim.g.ale_linters_ignore = { "phpstan" }
+	end,
 }
